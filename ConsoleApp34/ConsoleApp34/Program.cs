5796327345c89b3797a83713d77458e7fa357649
plaskina46 <118421132+plaskina46@users.noreply.github.com>
2022-11-22 08:22:14 +0300

    Add files via upload

---
+++ ConsoleApp34/ConsoleApp34/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] nums = new int[6];
+try
+{
+    Console.WriteLine("До генерирования исключения.");
+    for (int i = 0; i < 10; i++)
+    {
+        nums[i] = i;
+        Console.WriteLine("nums[(0)]: {1}", i, nums[i]);
+    }
+    Console.WriteLine("He подлежит выводу");
+}
+catch (IndexOutOfRangeException)
+{
+    Console.WriteLine("Индекс вышел за границы массива!");
+}
+Console.WriteLine("После блока перехвата исключения.");
